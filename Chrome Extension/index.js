f1eda439b31510d769f4722cb93965dddc8a7abc
Pratyush <pratyush.ks2003@gmail.com>
2023-06-03 23:47:07 +0530

    version: 3.0 added SAVE TAB button

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -5,31 +5,45 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const localList = JSON.parse(localStorage.getItem("List"))
+const tabBtn = document.getElementById("tab-btn")
 
 // fetch and display list if localStorage has inputs
 if (localList) {
     myList = localList 
     displayList()
 }
+
+// event listeners
 inputBtn.addEventListener("click", addList)
 inputEl.addEventListener("keypress", function(event){
     if (event.code === "Enter")
         addList()
 })
+tabBtn.addEventListener("click", addTab)
 deleteBtn.addEventListener("click", deleteList)
 
+// functions
 function addList() {
     myList.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("List", JSON.stringify(myList))
     displayList()  
 }
+
+function addTab() {
+    chrome.tabs.query({active: true, currentWindow: true, function(tabs) {
+        myList.push(tabs[0].url)
+        localList.setItem("List", JSON.stringify(myList))
+        displayList()
+    } })
+}
+
 function deleteList() {
-    alert("Are you sure you want to delete List?")
     localStorage.removeItem("List")
     myList = []
     displayList()
 }
+
 function displayList() {
     let listHTML = ""
     for (let i=0; i<myList.length; i++) {
